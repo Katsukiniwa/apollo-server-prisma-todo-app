cab7c1fde0814a093868752ecc4d240cea00d349
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-12-08 19:44:29 +0900

    create getTodos

---
+++ src/resolvers/query/getTodos.ts
@@ -0,0 +1,25 @@
+import { prisma } from '../../lib/prisma';
+import { QueryResolvers } from '../../types/generated/graphql';
+
+export const getTodos: QueryResolvers['getTodos'] = async (
+  parent,
+  args,
+  context,
+  info
+) => {
+  if (!context.user) {
+    console.error('user undefined')
+    // throw new Error('user undefined')
+  }
+  const todos = await prisma.todo.findMany({
+    where: {
+      userId: context.user?.id,
+    },
+    include: {
+      user: true,
+    },
+  });
+  console.log(context.user)
+  console.log(todos.length)
+  return todos;
+};
